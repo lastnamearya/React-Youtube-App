819fdafb717334782a3099b5d5279424d5a7844d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-30 03:46:54 +0530

    default video url set in the default props

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import ReactPlayer from "react-player";
 
 class Player extends Component {
+  static propTypes = {
+    url: PropTypes.string
+  };
+
+  static defaultProps = {
+    url: "https://www.youtube.com/watch?v=kz3nVya45uQ"
+  };
+
   render() {
-    return (
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-        className="Player"
-      />
-    );
+    return <ReactPlayer url={this.props.url} className="Player" />;
   }
 }
 
