ef087d272951787077fad0f39bc241817e76ec92
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-30 14:47:49 +0530

    Youtube API search added & tested

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import React, { Component } from "react";
 import Header from "./Components/Header";
 import Player from "./Components/Player";
 import Searchbar from "./Components/Searchbar";
+import searchYoutube from "youtube-api-search";
 import "./App.css";
 
+const YoutubeAPI = "AIzaSyB_AMcpW4h_F3BkYsxFYQnCRY2KDj09F5s";
+
 class App extends Component {
+  state = {
+    search: "React Conf",
+    results: []
+  };
+
+  searchResults = () => {
+    searchYoutube(
+      { key: YoutubeAPI, term: this.state.search },
+      function(data) {
+        console.log(data);
+        this.setState({
+          results: data
+        });
+      }.bind(this)
+    );
+  };
+
+  componentDidMount() {
+    this.searchResults();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="App">
